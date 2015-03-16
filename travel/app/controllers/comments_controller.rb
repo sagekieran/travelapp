31d7cf029613c9702798fabc1996ca2e9e54edc7
Sage McGill <sagekieran@gmail.com>
2015-03-16 14:15:24 -0400

    params fix

--- travel/app/controllers/comments_controller.rb
+++ travel/app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   end
 
   def suggestion_comments
-    @comments = Comment.find(suggestion_id: params(:suggestion_id))
+    @comments = Comment.find(suggestion_id: params[:suggestion_id])
     respond_to do |format|
       format.json { render json: @comments }
     end
