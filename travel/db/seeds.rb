ab0bfd5050c4c6592e41c2641187d9d7c90a5404
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-16 13:32:22 -0400

    fixed seed errors

--- travel/db/seeds.rb
+++ travel/db/seeds.rb
@@ -16,9 +16,16 @@ User.create([
 ])
 
 Trip.create([
-  { group_id: Group.find_by(user_id: "1"), user_id: User.find_by(email: "tposs@gmail.com").id,       location: "Mexico", title: "Spring Break", title: "going to cancun and getting crazy, but not too crazy!", duration: "April 10, 2015 - April 14, 2015"},
-  { group_id: Group.find_by(user_id: "2"), user_id: User.find_by(email: "sagekieran@gmail.com").id,  location: "Mexico", title: "Spring Break", title: "going to cancun and getting crazy, but not too crazy!", duration: "April 10, 2015 - April 14, 2015"},
-  { group_id: Group.find_by(user_id: "3"), user_id: User.find_by(email: "mikerembach@gmail.com").id, location: "Mexico", title: "Spring Break", title: "going to cancun and getting crazy, but not too crazy!", duration: "April 10, 2015 - April 14, 2015"},
-  { group_id: Group.find_by(user_id: "4"), user_id: User.find_by(email: "sadestevens@gmail.com").id, location: "Mexico", title: "Spring Break", title: "going to cancun and getting crazy, but not too crazy!", duration: "April 10, 2015 - April 14, 2015"}
+  { group_id: Group.find_by(user_id: "4"), user_id: User.find_by(email: "sadestevens@gmail.com").id, location: "Mexico", title: "Spring Break", description: "going to Mexico City and getting crazy, but not too crazy!", duration: "April 10, 2015 - April 14, 2015"}
 ])
 
+Suggestion.create([
+  { user_id: User.find_by(email: "tposs@gmail.com").id,      category_id: "1", title: "Food",      title: "Spring Break", title: "Los Caballos",       content: "amazingly delicious according to reviews",
+    link: "http://shechive.files.wordpress.com/2011/07/mexican-food-14.jpg",                                                                 upvote: "0", downvote: "0"},
+  { user_id: User.find_by(email: "sagekieran@gmail.com").id, category_id: "2", title: "lodging",   title: "Spring Break", title: "Hostel Airport Mexico DF", content: "we can stay here, it's a cool place to meet other travelers",
+    link: "http://u.hwstatic.com/propertyimages/4/41518/2.jpg",                                                                              upvote: "0", downvote: "0"},
+  { user_id: User.find_by(email: "mikerembach@gmail.com").id,category_id: "3", title: "Adventure", title: "Spring Break", title: "Beach",              content: "this is the best beach & closest to the city, let's do this the first day!?",
+    link: "http://activerain.com/image_store/uploads/4/8/8/9/8/ar123636422589884.jpg",                                                       upvote: "0", downvote: "0"},
+  { user_id: User.find_by(email: "sadestevens@gmail.com").id,category_id: "3", title: "Adventure", title: "Spring Break", title: "Horse riding tour",        content: "this place looks pretty cool from the pictures, we should check it out at some point",
+    link: "http://www.dosomethingdifferent.com/images/store/experience/landscape_image/1828/Horseback_Riding_In_The_Jungle8.jpg?1308854312", upvote: "0", downvote: "0"}
+])
